c14f46f9109e0c7d8fd6d051dd5fded0f6c65665
tomasbarak <barakcinthia@gmail.com>
2021-12-05 18:56:06 -0300

    Added allPublications endpoint

--- src/config/configRoutes.js
+++ src/config/configRoutes.js
@@ -2,6 +2,7 @@
 function config(app, firebaseApp, database, firebaseadmin){
     require('../api/routes/accountProfile').init(app, firebaseApp, database);
     require('../api/routes/accountStats').init(app, firebaseApp, database);
+    require('../api/routes/allPublications').init(app, firebaseApp, database);
     require('../api/routes/accountDisplayName').init(app, firebaseadmin);
     require('../api/routes/accountPhotoUrl').init(app, firebaseadmin);
 }
