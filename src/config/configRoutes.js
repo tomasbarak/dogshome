9afec3115e9c8c65cd1838554046d1213260fe39
tomasbarak <barakcinthia@gmail.com>
2021-12-05 19:24:26 -0300

    Added publicationById endpoint

--- src/config/configRoutes.js
+++ src/config/configRoutes.js
@@ -3,8 +3,8 @@ function config(app, firebaseApp, database, firebaseadmin){
     require('../api/routes/accountProfile').init(app, firebaseApp, database);
     require('../api/routes/accountStats').init(app, firebaseApp, database);
     require('../api/routes/allPublications').init(app, firebaseApp, database);
+    require('../api/routes/publicationById').init(app, firebaseApp, database);
     require('../api/routes/accountDisplayName').init(app, firebaseadmin);
     require('../api/routes/accountPhotoUrl').init(app, firebaseadmin);
-    require('../api/routes/publicationById').init(app, firebaseadmin);
 }
 module.exports = {config};
