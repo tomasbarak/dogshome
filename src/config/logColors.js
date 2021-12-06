438cbc05cb8e0deec9473d0c2e564db4651b0342
tomasbarak <barakcinthia@gmail.com>
2021-12-06 18:59:18 -0300

    Ashe

--- src/config/logColors.js
+++ src/config/logColors.js
@@ -1,13 +1,13 @@
 const logColor = {  
-    'black': '\x1b[30m%s\x1b[0m',
-    'error': '\x1b[31m%s\x1b[0m',
-    'success': '\x1b[32m%s\x1b[0m',
-    'warn': '\x1b[33m%s\x1b[0m',
-    'blue': '\x1b[34m%s\x1b[0m',
-    'magenta': '\x1b[35m%s\x1b[0m',
-    'debug': '\x1b[36m%s\x1b[0m',
-    'white': '\x1b[37m%s\x1b[0m',
-    'reset': '\x1b[0m%s\x1b[0m',
+    'black':    '\x1b[30m%s\x1b[0m',
+    'error':    '\x1b[31m%s\x1b[0m',
+    'success':  '\x1b[32m%s\x1b[0m',
+    'warn':     '\x1b[33m%s\x1b[0m',
+    'blue':     '\x1b[34m%s\x1b[0m',
+    'magenta':  '\x1b[35m%s\x1b[0m',
+    'debug':    '\x1b[36m%s\x1b[0m',
+    'white':    '\x1b[37m%s\x1b[0m',
+    'reset':    '\x1b[0m%s\x1b[0m',
   }
   
   module.exports = logColor;
