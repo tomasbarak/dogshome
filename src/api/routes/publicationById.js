83f5765053ea3d18e4da9eacddd8fef1fb23411c
tomasbarak <barakcinthia@gmail.com>
2021-12-06 01:37:07 -0300

    Added get single publication without state endpoint

--- src/api/routes/publicationById.js
+++ src/api/routes/publicationById.js
@@ -5,6 +5,28 @@ const logColor =        require(appDir + '/src/config/logColors');
 //Route to get all publications
 function init(app, firebaseApp, database){
     app.get('/publications/:state/:id/', function (req, res) {
+        console.log(logColor.debug, 'Publication ' + req.params.state + '/' + req.params.id + 'accessed by', req.headers['x-forwarded-for'] || req.connection.remoteAddress.split(":").pop());
+
+        const db =          database.getDatabase(firebaseApp);
+        const pubId =       req.params.id;
+        const dbRef =       database.ref(db);
+        const pubState =    req.params.state;
+        const child =       database.child;
+        const get =         database.get;
+
+        get(child(dbRef, `Publications/${pubState}/${pubId}/`)).then((snapshot) => {
+
+            res.header('Access-Control-Allow-Origin', '*');
+            res.status(200).send(snapshot.val());
+
+        }).catch((error) => {
+
+            res.status(500).send(error);
+
+          });
+
+    })
+    app.get('/publications/:id/', function (req, res) {
         console.log(logColor.debug, 'Publication ' + req.params.id + 'accessed by', req.headers['x-forwarded-for'] || req.connection.remoteAddress.split(":").pop());
 
         const db =          database.getDatabase(firebaseApp);
@@ -14,7 +36,7 @@ function init(app, firebaseApp, database){
         const child =       database.child;
         const get =         database.get;
 
-        get(child(dbRef, `Publications/${pubState}/${pubId}/`), database.limitToLast(2)).then((snapshot) => {
+        get(child(dbRef, `Publications/All/${pubId}/`)).then((snapshot) => {
 
             res.header('Access-Control-Allow-Origin', '*');
             res.status(200).send(snapshot.val());
