54399774465d6e877bc8243fe50fa595086e49f3
tomasbarak <barakcinthia@gmail.com>
2021-12-05 22:55:10 -0300

    Added publicationById endpoint

--- src/api/routes/publicationById.js
+++ src/api/routes/publicationById.js
@@ -3,8 +3,8 @@ function init(app, firebaseApp, database){
     app.get('/publications/:state/:id/', function (req, res) {
         console.log('ProfileStats accessed by', req.headers['x-forwarded-for'] || req.connection.remoteAddress.split(":").pop());
         const db =     database.getDatabase(firebaseApp);
-        const dbRef =  database.ref(db);
         const pubId = req.params.id;
+        const dbRef =  database.ref(db).equalTo(pubId);
         const pubState = req.params.state;
         const child = database.child;
         const get = database.get;
