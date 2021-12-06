0c9b0e7696ee2760e67faf618fdbba76a1930630
tomasbarak <barakcinthia@gmail.com>
2021-12-06 18:24:41 -0300

    Ashe

--- src/api/listeners/githubPushListener.js
+++ src/api/listeners/githubPushListener.js
@@ -7,6 +7,9 @@ const logColor = require('../../config/logColors');
 function listen(app){
     app.post('/webhooks/github/', function(req, res){
         if(req.headers['x-github-event'] == 'push'){
+
+            console.log(logColor.warn, 'Trying to merge changes from github');
+
             exec(`cd ${appDir} && git reset --hard && git pull && npm i && sudo pm2 flush app && sudo pm2 restart app`, (error, stdout, stderr) => {
                 if (!error) {
                     console.log(logColor.success, 'Successfully updated the app. Commit id: ' + req.body.head_commit.id);
