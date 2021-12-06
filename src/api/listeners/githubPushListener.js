f95f41efb135054d37ab61d641f17065fa8dd9f8
tomasbarak <barakcinthia@gmail.com>
2021-12-06 02:42:36 -0300

    A

--- src/api/listeners/githubPushListener.js
+++ src/api/listeners/githubPushListener.js
@@ -1,9 +1,21 @@
 
 //Route to listen to github webhooks
+const { dirname } =     require('path');
+const appDir =          dirname(require.main.filename);
+const { exec } =        require("child_process");
+
 function listen(app){
     app.post('/webhooks/github/', function(req, res){
-        console.log(req.headers);
-        res.send('ok');
+        if(req.headers['x-github-event'] == 'push'){
+            exec(`cd ${appDir} && git pull`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+            })
+        }
     })
 }
 
