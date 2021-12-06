6b365b9717e519456fff4b7fa6b1ab1ebdf2b48f
tomasbarak <barakcinthia@gmail.com>
2021-12-06 02:36:51 -0300

    A

--- src/api/listeners/githubPushListener.js
+++ src/api/listeners/githubPushListener.js
@@ -1,7 +1,7 @@
 
 //Route to listen to github webhooks
 function listen(app){
-    app.post('/webhooks/github', function(req, res){
+    app.post('/webhooks/github/', function(req, res){
         console.log(req.body);
         res.send('ok');
     })
