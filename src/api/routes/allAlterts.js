482853e86070472a80a100b38d54c79bec69adfb
tomasbarak <barakcinthia@gmail.com>
2021-12-06 02:04:13 -0300

    Added anti-ddos system

---
+++ src/api/routes/allAlterts.js
@@ -0,0 +1,29 @@
+const { dirname } =     require('path');
+const appDir =          dirname(require.main.filename);
+const logColor =        require(appDir + '/src/config/logColors');
+
+//Route to get all publications
+function init(app, firebaseApp, database){
+    app.get('/alerts/all/', function (req, res) {
+
+        console.log(logColor.debug, 'All alerts accessed by', req.headers['x-forwarded-for'] || req.connection.remoteAddress.split(":").pop());
+
+        const db =              database.getDatabase(firebaseApp);
+        const recentPostsRef =  database.query(database.ref(db, 'Alerts/All'), database.limitToLast(50));
+        const get =             database.get;
+
+        get(recentPostsRef).then((snapshot) => {
+
+            res.header('Access-Control-Allow-Origin', '*');
+            res.status(200).send(snapshot.val());
+
+        }).catch((error) => {
+
+            res.status(500).send(error);
+
+          });
+
+    })
+}
+
+module.exports = {init};
