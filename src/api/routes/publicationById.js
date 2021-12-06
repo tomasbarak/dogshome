4dd0bf1872fda078164ace08dd9ddecabe5adf7f
tomasbarak <barakcinthia@gmail.com>
2021-12-05 22:45:47 -0300

    Added publicationById endpoint

--- src/api/routes/publicationById.js
+++ src/api/routes/publicationById.js
@@ -1,14 +1,15 @@
 //Route to get all publications
 function init(app, firebaseApp, database){
-    app.get('/publications/:id/', function (req, res) {
+    app.get('/publications/:state/:id/', function (req, res) {
         console.log('ProfileStats accessed by', req.headers['x-forwarded-for'] || req.connection.remoteAddress.split(":").pop());
         const db =     database.getDatabase(firebaseApp);
         const dbRef =  database.ref(db);
         const pubId = req.params.id;
+        const pubState = req.params.state;
         const child = database.child;
         const get = database.get;
 
-        get(child(dbRef, `Publications/${pubId}/`)).then((snapshot) => {
+        get(child(dbRef, `Publications/${pubState}/${pubId}/`)).then((snapshot) => {
             res.header('Access-Control-Allow-Origin', '*');
             res.status(200).send(snapshot.val());
 
