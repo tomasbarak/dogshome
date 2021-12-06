add869c7680d5eb7c8f7317bf93ac06228315df1
tomasbarak <barakcinthia@gmail.com>
2021-12-06 18:22:54 -0300

    Ashe

--- src/api/listeners/githubPushListener.js
+++ src/api/listeners/githubPushListener.js
@@ -7,14 +7,10 @@ const logColor = require('../../config/logColors');
 function listen(app){
     app.post('/webhooks/github/', function(req, res){
         if(req.headers['x-github-event'] == 'push'){
-            exec(`cd ${appDir} && git reset --hard && git pull && npm i && sudo pm2 restart app`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(logColor.error, `Error updating: ${error}`);
-                    return;
+            exec(`cd ${appDir} && git reset --hard && git pull && npm i && sudo pm2 flush app && sudo pm2 restart app`, (error, stdout, stderr) => {
+                if (!error) {
+                    console.log(logColor.success, 'Successfully updated the app. Commit id: ' + req.body.head_commit.id);
                 }
-                console.log(logColor.warn, `stdout: ${stdout}`);
-                console.log(logColor.warn, `stderr: ${stderr}`);
-                console.log(logColor.success, `Successfully updated`);
             })
         }
     })
