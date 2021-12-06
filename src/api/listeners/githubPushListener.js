ecd8f49449d135b4c188122c9f7032c175781937
tomasbarak <barakcinthia@gmail.com>
2021-12-06 18:28:46 -0300

    Ashe

--- src/api/listeners/githubPushListener.js
+++ src/api/listeners/githubPushListener.js
@@ -8,9 +8,9 @@ function listen(app){
     app.post('/webhooks/github/', function(req, res){
         if(req.headers['x-github-event'] == 'push'){
 
-            console.log(logColor.warn, 'Trying to merge changes from github');
+            console.log(logColor.warn, 'Trying to merge changes from github. Commit id: ' + req.body.head_commit.id);
 
-            exec(`cd ${appDir} && git reset --hard && git pull && npm i && sudo pm2 flush app && sudo pm2 restart app`, (error, stdout, stderr) => {
+            exec(`sudo -su cd ${appDir} && git reset --hard && git pull && npm i && pm2 flush app && pm2 restart app`, (error, stdout, stderr) => {
                 if (!error) {
                     console.log(logColor.success, 'Successfully updated the app. Commit id: ' + req.body.head_commit.id);
                 }else{
