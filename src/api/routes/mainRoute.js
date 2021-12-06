47e2166d31a2f2ac77911315d8f1af9ce562077e
tomasbarak <barakcinthia@gmail.com>
2021-12-06 02:20:24 -0300

    Added html response for root get request

---
+++ src/api/routes/mainRoute.js
@@ -0,0 +1,10 @@
+const { dirname } =     require('path');
+const appDir =          dirname(require.main.filename);
+
+function init(app){
+    app.get('/', function(req, res){
+        res.sendFile(appDir + '/src/web/index.html');
+    })
+    
+}
+module.exports = {init};
