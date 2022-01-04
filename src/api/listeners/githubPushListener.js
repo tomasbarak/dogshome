e7386f543ee2e91af9d68c8592b265f5d6420931
tomasbarak <barakcinthia@gmail.com>
2022-01-04 02:36:59 -0300

    Added html routes

--- src/api/listeners/githubPushListener.js
+++ src/api/listeners/githubPushListener.js
@@ -10,7 +10,7 @@ function listen(app){
 
             console.log(logColor.blue, 'Trying to merge changes from github. Commit id: ' + req.body.head_commit.id);
             
-                exec(`sudo -su barak cd ${appDir} && git reset --hard && git pull`, (error, stdout, stderr) => {
+                exec(`sudo -su barak cd ${appDir} && git reset --hard && git pull origin testing`, (error, stdout, stderr) => {
                     if (!error) {
 
                         console.log(logColor.success, 'Successfully merged changes from github');
