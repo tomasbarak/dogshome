08b598fe0f2ee84ea9dba35303db7a9e99c77eff
tomasbarak <barakcinthia@gmail.com>
2022-01-09 02:13:36 -0300

    Added functional redirects

--- public/scripts/auth/sign-out.js
+++ public/scripts/auth/sign-out.js
@@ -1,17 +1,9 @@
-function signOut(){
-    //Make an xml http request to the server to sign out
-    //Make an xml http request to the server to sign out
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    }
-    xmlhttp.open("POST", "https://api.softvisiondevelop.com.ar/signout", true);
-
-    xmlhttp.send();
+function signOut() {
+    axios.post("/signout", {}).then(function (response) {
+        window.location.href = "/signin";
+    })
 }
 
-function deleteAllFlags(){
+function deleteAllFlags() {
     localStorage.clear()
 }
