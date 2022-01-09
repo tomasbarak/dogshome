31624a4b8bbbf79d678cf1ffb17f063de94a5f26
tomasbarak <barakcinthia@gmail.com>
2022-01-09 01:06:51 -0300

    Added singout endpoint

--- public/scripts/auth/sign-out.js
+++ public/scripts/auth/sign-out.js
@@ -1,12 +1,13 @@
 function signOut(){
-    firebase.auth().signOut().then(() => {
-        deleteAllFlags();
-        sessionStorage.clear();
-        localStorage.clear()
-        window.location.href = "signin.html";
-    }).catch((error) => {
-        // An error happened.
-    });
+    //Make an xml http request to the server to sign out
+    //Make an xml http request to the server to sign out
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    xmlhttp.open("POST", "https://api.softvisiondevelop.com.ar/signout", true);
 }
 
 function deleteAllFlags(){
