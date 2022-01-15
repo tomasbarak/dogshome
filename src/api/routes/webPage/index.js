bb8547db8581c562f0553323905ab38997152119
tomasbarak <barakcinthia@gmail.com>
2022-01-15 03:27:48 -0300

    Added partial anonym support

--- src/api/routes/webPage/index.js
+++ src/api/routes/webPage/index.js
@@ -6,44 +6,49 @@ function init(app, firebaseAdmin, firebaseApp, database) {
     //Setting up index route
     app.get(['/', '/index', '/index.html'], (req, res) => {
         const token = req.cookies.session || ' ';
-        if (token) {
-            firebaseAdmin.auth().verifySessionCookie(token, true /** checkRevoked */)
-                .then((decodedIdToken) => {
-                    if (decodedIdToken.email_verified) {
-                        let parsedDisplayName = JSON.parse(decodedIdToken.name);
-                        const db = database.getDatabase(firebaseApp);
-                        const recentPostsRef = database.query(database.ref(db, 'Publications/All'), database.limitToLast(50));
-                        const get = database.get;
 
-                        get(recentPostsRef).then((snapshot) => {
-                            var json_data = snapshot.val();
-                            var result = [];
+        const db = database.getDatabase(firebaseApp);
+        const recentPostsRef = database.query(database.ref(db, 'Publications/All'), database.limitToLast(50));
+        const get = database.get;
 
-                            for (var i in json_data) {
-                                result.push([json_data[i]]);
-                            }
-                            res.render(appDir + '/public/index', {
-                                uid: decodedIdToken.user_id,
-                                displayName: parsedDisplayName.nameAndSurname.displayName,
-                                name: parsedDisplayName.nameAndSurname.name,
-                                surname: parsedDisplayName.nameAndSurname.surname,
-                                photoUrl: decodedIdToken.picture,
-                                publications: result
-                            });
-                        }).catch((error) => {
-                            res.status(500).send(error);
-                        });
+        get(recentPostsRef).then((snapshot) => {
+            var json_data = snapshot.val();
+            var result = [];
 
-                    }else{
-                        res.redirect('/verification');
-                    }
+            for (var i in json_data) {
+                result.push([json_data[i]]);
+            }
 
-                }).catch((error) => {
-                    res.redirect('/signin');
+            firebaseAdmin.auth().verifySessionCookie(token, true /** checkRevoked */).then((decodedIdToken) => {
+                if (decodedIdToken.email_verified) {
+                    let parsedDisplayName = JSON.parse(decodedIdToken.name);
+                    res.render(appDir + '/public/index', {
+                        uid: decodedIdToken.user_id,
+                        displayName: parsedDisplayName.nameAndSurname.displayName,
+                        name: parsedDisplayName.nameAndSurname.name,
+                        surname: parsedDisplayName.nameAndSurname.surname,
+                        photoUrl: decodedIdToken.picture,
+                        publications: result,
+                        isPrivate: false
+                    });
+                } else {
+                    res.redirect('/verification');
+                }
+            }).catch((error) => {
+                res.render(appDir + '/public/index', {
+                    uid: '',
+                    displayName: 'Cuenta Privada',
+                    name: 'Cuenta',
+                    surname: 'Privada',
+                    photoUrl: 'https://dogshome.com.ar/profile/image/uploaded/default-private-user-image.png',
+                    publications: result,
+                    isPrivate: true
                 });
-        } else {
-            res.redirect('/signin');
-        }
+            });
+
+        }).catch((error) => {
+            res.status(500).send(error);
+        });
 
     });
 }
