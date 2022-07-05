629810bb461d49a0575815638c96ef9fa51d52e3
tomasbarak <barakcinthia@gmail.com>
2022-07-05 12:03:01 -0300

    Now, you can upload location department

--- src/api/routes/publicationDraft.js
+++ src/api/routes/publicationDraft.js
@@ -223,20 +223,24 @@
     
                                         //Handle draft step 3 (Dog Location)
                                         case 3:
-                                            const provincesArr =    ['Ciudad de Buenos Aires', 'Buenos Aires', 'Catamarca', 'Chaco', 'Chubut', 'Cordoba', 'Corrientes', 'Entre Rios', 'Formosa', 'Jujuy', 'La Pampa', 'La Rioja', 'Mendoza', 'Misiones', 'Neuquen', 'Rio Negro', 'Salta', 'San Juan', 'San Luis', 'Santa Cruz', 'Santa Fe', 'Santiago del Estero', 'Tierra del Fuego', 'Tucuman']
+                                            const provincesArr = [ "ciudad_de_buenos_aires", "buenos_aires", "catamarca", "chaco", "chubut", "cordoba", "corrientes", "entre_rios", "formosa", "jujuy", "la_pampa", "la_rioja", "mendoza", "misiones", "neuquen", "rio_negro", "salta", "san_juan", "san_luis", "santa_cruz", "santa_fe", "santiago_del_estero", "tierra_del_fuego", "tucuman" ];
                                             const province =        req.body.province;
-                                            const provinceName =    provincesArr[province];
-    
-                                            //Check if the draft province is NOT empty
-                                            if(province.length > 0){
+                                            const department =      String(req.body.department).toLowerCase().replace(/ /g, '_');
+                                            const provinceName =    String(provincesArr[province]).toLowerCase().replace(/ /g, '_');
+                                            const departmentArr = require(`../../../public/draft-steps/departments/${provinceName}.json`).departamentos.map((value) => {
+                                                return value.nombre.toLowerCase().replace(/ /g, '_');
+                                            });
+
+                                            //Check if the draft province is NOT empty and if the draft department is NOT empty and if the draft department is in the 
+                                            if(province.length > 0 && department.length > 0 && departmentArr.includes(department)){
                                                 newDraft["Province"] =  provinceName;
+                                                newDraft["Department"] = department;
                                                 newDraft["updatedAt"] = new Date();
                                                 newDraft["Step"] =      4;
-    
                                                 continueUpdating()
                                                 break;
                                             }else{
-                                                res.status(400).send({error: 'Province is required.'});
+                                                res.status(400).send({error: 'Error in your request'});
                                                 client.close();
                                                 return;
                                             }
