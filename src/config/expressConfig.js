2a3f67a2fce8642fc3c12667c641ef573f300752
= <=>
2022-12-28 01:24:48 -0300

    TESTING Removed Anti-DDOS

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -24,7 +24,7 @@ function config(express, cors){
     app.use(cors());
     app.use(bodyParser.json({ limit: '30mb', extended: true }))
     app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-    app.use(anti_ddos.express)
+    //app.use(anti_ddos.express)
 
     return app;
 }
