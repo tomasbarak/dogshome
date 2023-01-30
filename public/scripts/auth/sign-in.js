1ba4a1f391a20bb4903197148ac5d91792087285
= <=>
2023-01-30 03:50:45 -0300

    Changed login animation

--- public/scripts/auth/sign-in.js
+++ public/scripts/auth/sign-in.js
@@ -37,115 +37,71 @@ function signIn(email, password) {
             return null;
         }
     }
-    
-    if (email.length > 0 && password.length > 0) {
-        Swal.fire({
-            title: 'Espere por favor',
-            text: 'Estamos iniciando su sesión',
-            didOpen: () => {
-                Swal.showLoading();
-            },
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            heightAuto: false,
-            allowEnterKey: false,
-        });
-        firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) => {
-            // Signed in
-            let user = userCredential.user;
 
-            user.getIdToken(true).then(async function (idToken) {
-                try {
-                    const subscription = await getPushSubscription();
-                    axios.post(`https://api.${window.location.hostname}/auth/login`, {
-                        idToken: idToken,
-                        subscription: subscription
-                    }, {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        withCredentials: true
-                    }).then(function (response) {
-                        if(response.status == 200) {
-                            window.location.href = '/';
-                        } else {
-                            Swal.fire({
-                                title: 'Error',
-                                text: 'Ha ocurrido un error',
-                                heightAuto: false,
-                                icon: 'error',
-                                confirmButtonText: 'Reintentar',
-                                confirmButtonColor: '#079292'
-                            });
-                        }
-                    }).catch(function (error) {
-                        console.log(error);
-                    });
-                } catch (error) {
-                    axios.post(`https://api.${window.location.hostname}/auth/login`, {
-                        idToken: idToken,
-                        subscription: null
-                    }, {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        withCredentials: true
-                    }).then(function (response) {
-                        if(response.status == 200) {
-                            window.location.href = '/';
-                        } else {
-                            Swal.fire({
-                                title: 'Error',
-                                text: 'Ha ocurrido un error',
-                                heightAuto: false,
-                                icon: 'error',
-                                confirmButtonText: 'Reintentar',
-                                confirmButtonColor: '#079292'
-                            });
-                        }
-                    }).catch(function (error) {
-                        console.log(error);
-                    });
+    return new Promise((resolve, reject) => {
+        if (email.length > 0 && password.length > 0) {
+            firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) => {
+                // Signed in
+                let user = userCredential.user;
+
+                user.getIdToken(true).then(async function (idToken) {
+                    try {
+                        const subscription = await getPushSubscription();
+                        axios.post(`https://api.${window.location.hostname}/auth/login`, {
+                            idToken: idToken,
+                            subscription: subscription
+                        }, {
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            withCredentials: true
+                        }).then(function (response) {
+                            if(response.status == 200) {
+                                resolve();
+                            } else {
+                                reject("Ha ocurrido un error");
+                            }
+                        }).catch(function (error) {
+                            console.log(error);
+                        });
+                    } catch (error) {
+                        axios.post(`https://api.${window.location.hostname}/auth/login`, {
+                            idToken: idToken,
+                            subscription: null
+                        }, {
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            withCredentials: true
+                        }).then(function (response) {
+                            if(response.status == 200) {
+                                resolve();
+                            } else {
+                                reject("Ha ocurrido un error");
+                            }
+                        }).catch(function (error) {
+                            reject(error);                            
+                        });
+                    }
+                }).catch(function (error) {
+                    reject(error);
+                });
+                localStorage.setItem("userDataMail", email);
+            }).catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                console.log("Error: " + errorMessage, errorCode);
+                if (errorCode == 'auth/wrong-password' || errorCode == 'auth/user-not-found') {
+                    reject("El correo o la contraseña son incorrectos");
+                } else if (errorCode == 'auth/invalid-email') {
+                    reject("El correo no es válido");
+                }else {
+                    reject("Ha ocurrido un error");
                 }
-            }).catch(function (error) {
-                console.error(error);
             });
-            localStorage.setItem("userDataMail", email);
-        }).catch((error) => {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            console.log("Error: " + errorMessage, errorCode);
-            if (errorCode == 'auth/wrong-password' || errorCode == 'auth/user-not-found') {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'El correo o la contraseña son incorrectos',
-                    icon: 'error',
-                    heightAuto: false,
-                    confirmButtonText: 'Reintentar',
-                    confirmButtonColor: '#079292'
-                });
-            } else if (errorCode == 'auth/invalid-email') {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'El correo no es válido',
-                    heightAuto: false,
-                    icon: 'error',
-                    confirmButtonText: 'Reintentar',
-                    confirmButtonColor: '#079292'
-                });
-            }else {
-                Swal.fire({
-                    title: 'Error',
-                    text: 'Ha ocurrido un error',
-                    heightAuto: false,
-                    icon: 'error',
-                    confirmButtonText: 'Reintentar',
-                    confirmButtonColor: '#079292'
-                });
-            }
-        });
-
-        
-    }
+        } else {
+            reject("Debes llenar todos los campos");
+        }
+    });
 
 }
