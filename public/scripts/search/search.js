c00b592f969093620ddc3d68b0af936a2b1ff1cd
tomasbarak <barakcinthia@gmail.com>
2023-04-25 12:41:41 -0300

    A

--- public/scripts/search/search.js
+++ public/scripts/search/search.js
@@ -1,23 +1,19 @@
 function search(text, filters = {}) {
     return new Promise((resolve, reject) => {
-        text = String(text).trim();
-        if (text == "") {
-            reject("No se ha ingresado ningún texto para buscar");
-        } else {
-            axios.post(`https://api.${window.location.hostname}/search`, {
-                query: text,
-                filters: filters,
-                page: 1
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                withCredentials: true
-            }).then((response) => {
-                resolve(response.data);
-            }).catch((error) => {
-                reject(error);
-            });
-        }
+        text = text || "";
+        axios.post(`https://api.${window.location.hostname}/search`, {
+            query: text !== "" ? text : '',
+            filters: filters,
+            page: 1
+        }, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            withCredentials: true
+        }).then((response) => {
+            resolve(response.data);
+        }).catch((error) => {
+            reject(error);
+        });
     });
 }
