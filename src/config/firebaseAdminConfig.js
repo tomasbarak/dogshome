531b8d2eb46181ba49ac27274f932303b108bb50
Tomás Barak <70740123+tomasbarak@users.noreply.github.com>
2023-07-04 17:35:47 -0300

    Update firebaseAdminConfig.js

--- src/config/firebaseAdminConfig.js
+++ src/config/firebaseAdminConfig.js
@@ -3,10 +3,12 @@
  */
 
 const firebaseAdmin = require('firebase-admin');
+const { dirname } =     require('path');
+const appDir =          dirname(require.main.filename);
 
 function config(){
     firebaseAdmin.initializeApp({
-        credential:     firebaseAdmin.credential.cert("./firebase-adminsdk.json"),
+        credential:     firebaseAdmin.credential.cert(`/usr/src/app/certs/firebase-adminsdk.json`),
         databaseURL:    "https://dogshome-6af88-default-rtdb.firebaseio.com"
     });
     
