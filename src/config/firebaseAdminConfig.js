770efd9c4704f9b36d393845ace00c2e6281f4e9
tomasbarak <barakcinthia@gmail.com>
2023-07-04 23:43:47 -0300

    Modified firebase-adminsdk.json location

--- src/config/firebaseAdminConfig.js
+++ src/config/firebaseAdminConfig.js
@@ -8,7 +8,7 @@ const appDir =          dirname(require.main.filename);
 
 function config(){
     firebaseAdmin.initializeApp({
-        credential:     firebaseAdmin.credential.cert(appDir + "/certs/firebase-adminsdk.json"),
+        credential:     firebaseAdmin.credential.cert(appDir + "/certs/firebase/firebase-adminsdk.json"),
         databaseURL:    "https://dogshome-6af88-default-rtdb.firebaseio.com"
     });
     
